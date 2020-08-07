bf0c49b7452e21fca7d8164ca4c8a6a319104bc5
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-08-07 15:22:59 -0300

    vars

--- index.php
+++ index.php
@@ -10,6 +10,34 @@ $vars= [
     'velocidade'=>'260Mbps / 260Mbps'
 ];
 
+$vars['logs'][0]['data'] = '2020-08-06 20:36:18';
+$vars['logs'][0]['log'] = 'Existing IP: 186.227.140.112 (did Mikrotik2 cli E4:18:6B:F7:09:E8 port 15729138 user eh.marschalk)';
+$vars['logs'][1]['data'] = '2020-08-06 20:36:18';
+$vars['logs'][1]['log'] = 'Existing IP: 186.227.140.112 (did Mikrotik2 cli E4:18:6B:F7:09:E8 port 15729138 user eh.marschalk)';
+
+
+$vars['auths'][0]['concentrador'] = 'Mikrotik_5';
+$vars['auths'][0]['inicio'] = '05/08/2020 00:44:07';
+$vars['auths'][0]['termino'] = '05/08/2020 01:13:21';
+$vars['auths'][0]['duracao'] = '00:30h';
+$vars['auths'][0]['trafego'] = '14.3GB';
+$vars['auths'][0]['motivo'] = 'Lost Carrier';
+$vars['auths'][0]['ipconection'] = '100.64.164.53';
+$vars['auths'][0]['ipconcentrador'] = '100.64.164.53';
+$vars['auths'][0]['ipv6'] = '';
+$vars['auths'][0]['mac'] = '68:FF:7B:83:93:EE';
+
+$vars['auths'][1]['concentrador'] = 'Mikrotik_5';
+$vars['auths'][1]['inicio'] = '05/08/2020 00:44:07';
+$vars['auths'][1]['termino'] = '05/08/2020 01:13:21';
+$vars['auths'][1]['duracao'] = '00:30h';
+$vars['auths'][1]['trafego'] = '14.3GB';
+$vars['auths'][1]['motivo'] = 'Lost Carrier';
+$vars['auths'][1]['ipconection'] = '100.64.164.53';
+$vars['auths'][1]['ipconcentrador'] = '100.64.164.53';
+$vars['auths'][1]['ipv6'] = '';
+$vars['auths'][1]['mac'] = '68:FF:7B:83:93:EE';
+
 if(isset($_GET['autenticacao']) AND $_GET['autenticacao'] == 1){
     $vars['autenticacao'] = 'true';
 }
@@ -17,7 +45,8 @@ if(isset($_GET['log']) AND $_GET['log'] == 1){
     $vars['log'] = 'true';
 }
 
+
+
 $template = $twig->load('radius-data.twig');
 
 echo $template->render($vars);
-dump($vars);
