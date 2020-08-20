8182ca3b318ced13585488ea869a7a302d40016b
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-08-20 09:12:57 -0300

    fix login

--- login.php
+++ login.php
@@ -12,10 +12,16 @@ $vars = [];
 if(isset($_POST['uname']) AND isset($_POST['psw'])){
     if(!empty($_POST['uname']) AND $_POST['uname'] != NULL){
         $user = UsuarioQuery::create()->findOneByLogin($_POST['uname']);
-        if ($user->getSenha() == md5($_POST['psw'])){
-            $_SESSION['logado'] = true;
-            header("location: login.php");
-            die();
+        if($user != null){
+            if(isset($_POST['psw'])){
+                if ($user->getSenha() == md5($_POST['psw'])){
+                    $_SESSION['logado'] = true;
+                    header("location: login.php");
+                    die();
+                } else {
+                    $vars['error'] = 1;
+                }
+            }
         } else {
             $vars['error'] = 1;
         }
