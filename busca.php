a9cfb8d755dcdc57363359b269a547fe3c38c22a
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-08-20 08:50:02 -0300

    icons

--- busca.php
+++ busca.php
@@ -12,7 +12,7 @@ $vars = [];
 if(isset($_GET['pppoe']) AND $_GET['pppoe'] != null){
     $vars['pppoe'] = $_GET['pppoe'];
 
-    $clientes = ClienteQuery::create()->findByPppoe($_GET['pppoe']);
+    $clientes = ClienteQuery::create()->where("pppoe like '%".$_GET['pppoe']."%'OR nome like '%".$_GET['pppoe']."%'");
     $counter = 0;
     foreach ($clientes as $cliente){
         $vars['clientes'][$counter]['nome'] = $cliente->getNome();
