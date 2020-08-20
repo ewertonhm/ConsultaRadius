104d309771d9ea27dd7d515b07435be24824b90b
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-08-20 15:54:57 -0300

    busca por cpf

--- busca.php
+++ busca.php
@@ -12,12 +12,15 @@ $vars = [];
 if(isset($_GET['pppoe']) AND $_GET['pppoe'] != null){
     $vars['pppoe'] = $_GET['pppoe'];
 
-    $clientes = ClienteQuery::create()->where("pppoe like '%".$_GET['pppoe']."%'OR nome like '%".$_GET['pppoe']."%'");
+    $clientes = ClienteQuery::create()->where("pppoe like '%".$_GET['pppoe']."%'OR nome like '%".$_GET['pppoe']."%' OR documento like '%".$_GET['pppoe']."%'");
     $counter = 0;
     foreach ($clientes as $cliente){
         $vars['clientes'][$counter]['nome'] = $cliente->getNome();
         $vars['clientes'][$counter]['pppoe'] = $cliente->getPppoe();
         $vars['clientes'][$counter]['id'] = $cliente->getId();
+        $vars['clientes'][$counter]['documento'] = $cliente->getDocumento();
+        $vars['clientes'][$counter]['cidade'] = $cliente->getCidade();
+        $vars['clientes'][$counter]['endereco'] = $cliente->getEndereco();
         $counter++;
     }
 }
